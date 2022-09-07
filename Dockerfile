92d3fd54002514fe10dc4af00f4fa264f81b5c36
sunman <shynhan@qq.com>
2022-09-07 22:27:48 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM debian:latest
 COPY [ "init.sh", "/" ]
-RUN chmod +x /init.sh
-RUN /init.sh
+RUN [ "/bin/bash", "/init.sh" ]
 ENV LANG=en_US.UTF-8 SHELL=/bin/zsh TZ=Asia/Shanghai PATH=/usr/local/go/bin:/root/go/bin:$PATH
 WORKDIR /workspace
 CMD [ "/bin/sleep", "infinity" ]
--- init.sh
+++ init.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 set -eux
 DEBIAN_FRONTEND=noninteractive
 
-alias install='apt-get install -y --no-install-recommends'
+function add() {
+  apt-get install -y --no-install-recommends $@
+}
 
-function clean {
+function clean() {
   rm -rf /var/lib/apt/lists/*
   rm $1
 }
 
-function base {
+function base() {
   apt-get update && apt-get upgrade -y
   install locales tzdata ca-certificates curl wget netbase gnupg dirmngr procps iputils-ping iproute2 nano sudo git openssh-client
   localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
   git config --global init.defaultBranch main
 }
 
-function install_zsh {
-  install zsh
+function install_zsh() {
+  add zsh
   chsh -s $(which zsh)
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
@@ -25,19 +27,19 @@ function install_zsh {
   echo "zstyle ':omz:update' mode disabled" >>~/.zshrc
 }
 
-function install_python {
-  install python3 python3-pip
+function install_python() {
+  add python3 python3-pip
 }
 
-function install_nodejs {
+function install_nodejs() {
   curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && apt-get install -y --no-install-recommends nodejs
   npm install -g npm pnpm
   npm -g cache clean --force
   rm /root/.npm/_logs/*.log
 }
 
-function install_go {
-  install g++ gcc libc6-dev make pkg-config
+function install_go() {
+  add g++ gcc libc6-dev make pkg-config
   wget $1 -O go.tar.gz
   rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
   export PAHT=/usr/local/go/bin:/root/go/bin:$PATH
